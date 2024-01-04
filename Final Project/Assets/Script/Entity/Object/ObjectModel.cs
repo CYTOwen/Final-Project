9bdd945177e9d9702ceee6b8a19020c2aa3ba69d
MuChengChen <oscar0905327634@gmail.com>
2024-01-04 14:42:40 +0800

    adjest prefab

--- Final Project/Assets/Script/Entity/Object/ObjectModel.cs
+++ Final Project/Assets/Script/Entity/Object/ObjectModel.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class ObjectModel : EntityModel
 {
     //protected float orginalSpeed { get { return speed; } }
-    protected float movingSpeed = 1f;
+    protected float movingSpeed = 50f;
     protected float timer = 0f;
     
     protected virtual void dropdown(GameObject ob)   //object drop down
     {
-        if (ob.transform.position.y > -3.7f)
+        if (ob.transform.position.y > -336f)
         {
             ob.transform.Translate(movingSpeed * Time.deltaTime * Vector2.down);
         }
@@ -19,7 +19,7 @@ public class ObjectModel : EntityModel
 
     protected virtual void when_object_auto_dissapear(ref float timer,GameObject gameObject) //when object drop to bottom,exist through a time and dissapear
     {
-        if (gameObject.transform.position.y <= -3.7f)
+        if (gameObject.transform.position.y <= -336f)
         {
             timer += Time.deltaTime;
         }
